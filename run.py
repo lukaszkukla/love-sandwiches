6b5dab06be34910c6d76e5e387177ff5663bf45e
lukaszkukla <lukaszkukla@gmail.com>
2022-03-28 21:48:47 +0000

    feat: calculate stock numbers and add to stock worksheet

--- run.py
+++ run.py
@@ -119,6 +119,22 @@ def get_last_5_entries_sales():
     return columns
 
 
+def calculate_stock_data(data):
+    """
+    calculate the average stock for each item type, add 10% and round up
+    """
+    print('calculating stock data... \n')
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
+
 
 def main():
     """
@@ -129,7 +145,9 @@ def main():
     update_worksheet(sales_data, 'sales')
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, 'surplus')
-    print(new_surplus_data)
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, 'stock')
     
 
 
@@ -142,6 +160,5 @@ print(
      )
 
 
-# main()
+main()
 
-sales_columns = get_last_5_entries_sales()
