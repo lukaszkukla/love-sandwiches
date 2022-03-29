39cfd62dbbd87beebefb241fcc084f37979d5be6
lukaszkukla <lukaszkukla@gmail.com>
2022-03-29 10:13:50 +0000

    feat: update requirements.txt for heroku deployment

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -26,7 +25,7 @@ def get_sales_data():
         print('data should be six numbers separated by commas')
         print('example: 10, 20, 30, 40, 50, 60\n')
 
-        data_str = input('enter your data here: ')
+        data_str = input('enter your data here: \n')
         
         sales_data = data_str.split(',')
                 
